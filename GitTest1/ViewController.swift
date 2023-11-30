d9fa4836833d43cf44dda94de587e8c38459f675
mikhail <Morochko2014@gmail.com>
2023-11-30 18:36:16 +0300

    Some changes

--- GitTest1/ViewController.swift
+++ GitTest1/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let label = UILabel()
-        label.text = "Hello!"
+        label.text = "Hello! 123"
 
         view.addSubview(label)
 
